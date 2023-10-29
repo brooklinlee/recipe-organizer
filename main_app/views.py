5f80f034c9bbc0a4a4de1d90e692b7e420ec0b14
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-29 17:36:12 -0600

    set up base route for index view of recipes

--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Recipe:
+  def __init__(self, name, cuisine, time, feeds):
+    self.name = name
+    self.cuisine = cuisine
+    self.time = time
+    self.feeds = feeds
+
+recipes= [
+  Recipe('Pizza', 'American', '1 Hour', '4'),
+  Recipe('Burger', 'American', '1 Hour', '4'),
+  Recipe('Cookie', 'American', '1 Hour', '1'),
+  Recipe('Popcorn', 'American', '1 Hour', '2')
+]
+
 # Create your views here.
 def home(request):
   return HttpResponse('<h1>Recipe Organizer Home Page</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def recipe_index(request):
+  return render(request, 'recipes/index.html', {'recipes': recipes})
