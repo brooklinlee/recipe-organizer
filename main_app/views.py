20ab8ad8f4c570235fd9183ef548df1717ead338
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-29 20:20:30 -0600

    add data to database for recipies, stub up home.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import render
+from .models import Recipe
 
 from django.http import HttpResponse
 
-class Recipe:
-  def __init__(self, name, cuisine, time, feeds):
-    self.name = name
-    self.cuisine = cuisine
-    self.time = time
-    self.feeds = feeds
+# class Recipe:
+#   def __init__(self, name, cuisine, time, feeds):
+#     self.name = name
+#     self.cuisine = cuisine
+#     self.time = time
+#     self.feeds = feeds
 
-recipes= [
-  Recipe('Pizza', 'American', '1 Hour', '4'),
-  Recipe('Burger', 'American', '1 Hour', '4'),
-  Recipe('Cookie', 'American', '1 Hour', '1'),
-  Recipe('Popcorn', 'American', '1 Hour', '2')
-]
+# recipes= [
+#   Recipe('Pizza', 'American', '1 Hour', '4'),
+#   Recipe('Burger', 'American', '1 Hour', '4'),
+#   Recipe('Cookie', 'American', '1 Hour', '1'),
+#   Recipe('Popcorn', 'American', '1 Hour', '2')
+# ]
 
 # Create your views here.
 def home(request):
-  return HttpResponse('<h1>Recipe Organizer Home Page</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
 
 def recipe_index(request):
+  recipes = Recipe.objects.all()
   return render(request, 'recipes/index.html', {'recipes': recipes})
