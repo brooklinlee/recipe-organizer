52db32a8075b7abbebaebea995211b647face320
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-30 17:21:02 -0600

    set up base recipe form html and css

--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Recipe
 from django.views.generic.edit import CreateView
 
-from django.http import HttpResponse
-
-# class Recipe:
-#   def __init__(self, name, cuisine, time, feeds):
-#     self.name = name
-#     self.cuisine = cuisine
-#     self.time = time
-#     self.feeds = feeds
-
-# recipes= [
-#   Recipe('Pizza', 'American', '1 Hour', '4'),
-#   Recipe('Burger', 'American', '1 Hour', '4'),
-#   Recipe('Cookie', 'American', '1 Hour', '1'),
-#   Recipe('Popcorn', 'American', '1 Hour', '2')
-# ]
-
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
