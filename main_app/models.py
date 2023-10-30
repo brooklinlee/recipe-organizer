9ef65f8018efe3ad91a358490f9f433e5a7bc4a1
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-10-29 20:00:42 -0600

    create Recipe class model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Recipe(models.Model):
+  name = models.CharField(max_length=100)
+  CUISINE_CHOICES = [
+    ('american', 'American'),
+    ('italian', 'Italian'),
+    ('mexican', 'Mexican'),
+    ('indian', 'Indian'),
+    ('asian', 'Asian'),
+    ('other', 'Other'),
+  ]
+  cuisine = models.CharField(
+    max_length=20,
+    choices=CUISINE_CHOICES,
+    default='american'
+  )
+  time = models.IntegerField()
+  feeds = models.IntegerField()
+  recipe = models.TextField(max_length = 500)
