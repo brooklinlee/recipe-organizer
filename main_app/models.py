b6d02332ea5525650aac2008b9e7b532ca7338a1
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-01 12:54:52 -0600

    update recipe model to add user and change time options

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
@@ -36,15 +37,29 @@ class Recipe(models.Model):
     ('asian', 'Asian'),
     ('other', 'Other'),
   ]
+  TIME_CHOICES = [
+    ('30 Minutes', '30 Minutes'),
+    ('1 Hour', '1 Hour'),
+    ('1.5 Hours', '1.5 Hours'),
+    ('2 Hours', '2 Hours'),
+    ('2.5 Hours', '2.5 Hours'),
+    ('Up to 6 Hours', 'Up to 6 Hours'),
+    ('More than 6 Hours', 'More than 6 Hours'),
+  ]
   cuisine = models.CharField(
     max_length=20,
     choices=CUISINE_CHOICES,
     default=CUISINE_CHOICES[0][0]
   )
-  time = models.IntegerField()
+  time = models.CharField(
+    max_length = 50,
+    choices=TIME_CHOICES,
+    default=TIME_CHOICES[0][0]
+  )
   feeds = models.IntegerField()
   recipe = models.TextField(max_length = 500)
   ingredients = models.ManyToManyField(Ingredient)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
   
   def __str__(self):
     return self.name
