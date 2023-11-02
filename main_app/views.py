7ee0646187b47fea45696e5b0617e633e075b54f
brooklinlee <141959146+brooklinlee@users.noreply.github.com>
2023-11-02 13:02:13 -0600

    fix singup bug

--- main_app/views.py
+++ main_app/views.py
@@ -84,7 +84,7 @@ def signup(request):
     if form.is_valid():
       user = form.save()
       login(request, user)
-      return redirect('cat-index')
+      return redirect('recipe-index')
     else:
       error_message = 'Invalid sign up - try again'
   form = UserCreationForm()
